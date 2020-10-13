119441045c3eec0c8bab4b253d2df17f97708614
Marco Méndez <marco.mendez.ing@gmail.com>
2020-10-13 18:26:17 -0500

    Test commit

--- app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/AnimationActivity.java
+++ app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/AnimationActivity.java
@@ -14,6 +14,7 @@ public class AnimationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //
         setContentView(R.layout.activity_animation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
