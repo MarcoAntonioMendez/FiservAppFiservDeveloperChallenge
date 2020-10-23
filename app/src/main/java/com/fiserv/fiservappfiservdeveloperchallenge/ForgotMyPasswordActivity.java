00cbadca7fb8623b8677b674c722d7bcfcb86daf
Marco Méndez <marco.mendez.ing@gmail.com>
2020-10-22 23:30:18 -0500

    Implemented ForgotMyPasswordActivity.java graphic interface.

--- app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/ForgotMyPasswordActivity.java
+++ app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/ForgotMyPasswordActivity.java
@@ -8,10 +8,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 public class ForgotMyPasswordActivity extends AppCompatActivity {
     public static final String NAVIGATION_BAR_COLOR = "#FE3412";
     public static final String STATUS_BAR_COLOR = "#363636";
+
+    private TextView dontWorryTetView;
+    private EditText emailEditText;
+    private Button sendEmailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,43 @@ public class ForgotMyPasswordActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getWindow().setNavigationBarColor(Color.parseColor(NAVIGATION_BAR_COLOR));
         getWindow().setStatusBarColor(Color.parseColor(STATUS_BAR_COLOR));
+
+        // Getting graphic elements references from XML file.
+        dontWorryTetView = findViewById(R.id.dont_worry_text_view_forgot_my_password_activity);
+        emailEditText = findViewById(R.id.enter_email_edit_text_forgot_my_password_activity);
+        sendEmailButton = findViewById(R.id.send_email_button_forgot_my_password_activity);
+
+        // Sets the text for all graphic components
+        setGraphicComponentsText();
+    }
+
+    /**
+     * Sets the text for all graphic components according to the language.
+     * Supported languages: English, Spanish.
+     */
+    private void setGraphicComponentsText(){
+        String language = Locale.getDefault().getLanguage();
+
+        switch(language){
+            case AppGlobalConstants.ENGLISH_LANGUAGE:
+                dontWorryTetView.setText("Don't worry if you forgot your password, just enter your" +
+                        " email we'll send you en email with the instructions to follow :D.");
+                emailEditText.setText("E-mail");
+                sendEmailButton.setText("Send");
+            break;
+            case AppGlobalConstants.SPANISH_LANGUAGE:
+                dontWorryTetView.setText("No te preocupes si olvidaste tu contraseña, sólo ingresa" +
+                        " tu correo electrónico y te enviaremos un correo electrónico con las " +
+                        " instrucciones a seguir :D.");
+                emailEditText.setText("Correo Electrónico");
+                sendEmailButton.setText("Enviar");
+            break;
+            default:
+                dontWorryTetView.setText("Don't worry if you forgot your password, just enter your" +
+                        " email we'll send you en email with the instructions to follow :D.");
+                emailEditText.setText("E-mail");
+                sendEmailButton.setText("Send");
+        }
     }
 
 }
