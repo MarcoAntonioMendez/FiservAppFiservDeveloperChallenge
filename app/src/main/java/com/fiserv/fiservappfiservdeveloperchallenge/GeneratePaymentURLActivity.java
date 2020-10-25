ddedd28af6f2972a8a42444e8f279438c199f3ff
Marco Méndez <marco.mendez.ing@gmail.com>
2020-10-25 01:27:49 -0500

    Implemented Architecture to send http petitions with RestClient.java; also implemented base code to generate payment URL.

--- app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/GeneratePaymentURLActivity.java
+++ app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/GeneratePaymentURLActivity.java
@@ -7,7 +7,34 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.fiserv.fiservappfiservdeveloperchallenge.banking.RestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.client.HttpResponseException;
 
 public class GeneratePaymentURLActivity extends AppCompatActivity {
     public static final String NAVIGATION_BAR_COLOR = "#FE3412";
@@ -21,8 +48,67 @@ public class GeneratePaymentURLActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getWindow().setNavigationBarColor(Color.parseColor(NAVIGATION_BAR_COLOR));
         getWindow().setStatusBarColor(Color.parseColor(STATUS_BAR_COLOR));
-
     }
 
+    private void generatePaymentURL(String total){
+        RestClient client = new RestClient(getApplicationContext());
+
+        try {
+            //Build JSON
+            JSONObject saleTransaction = new JSONObject();
+            saleTransaction.put("transactionAmount",
+                    (new JSONObject())
+                            .put("total", total)
+                            .put("currency", "MXN")
+            );
+            saleTransaction.put("transactionType","SALE");
+            saleTransaction.put("transactionNotificationURL","https://showmethepaymentresult.com");
+            saleTransaction.put("expiration","4102358400");
+            saleTransaction.put("authenticateTransaction","true");
+            saleTransaction.put("dynamicMerchantName","MyWebsite");
+            saleTransaction.put("invoiceNumber","96126098");
+            saleTransaction.put("purchaseOrderNumber","123055342");
+            saleTransaction.put("ip","264.31.73.24");
+            //send POST HTTP REQUEST
+            client.post("payment-url", saleTransaction, new JsonHttpResponseHandler(){
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    super.onSuccess(statusCode, headers, response);
+                    Log.d("SuccessObj", response.toString());
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    super.onSuccess(statusCode, headers, response);
+                    //handle success when response is an array of objects
+                    Log.d("SuccessArr", response.toString());
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
+                    //handle error when response is an array of objects
+                    Log.d("ErrorArr", errorResponse.toString());
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    super.onFailure(statusCode, headers, throwable, errorResponse);
+                    //handle success when response is a single object
+                    Log.d("ErrorObj", errorResponse.toString());
+                }
+            });
+
+        }catch (HttpResponseException ex){
+            Log.d("HttpResponseException", ex.getMessage());
+        }
+        catch (JSONException ex){
+            Log.d("JSONException", ex.getMessage());
+        }
+        catch (Exception ex){
+            Log.d("Exception", ex.getMessage());
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/banking/RestClient.java
@@ -0,0 +1,96 @@
+package com.fiserv.fiservappfiservdeveloperchallenge.banking;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.Base64;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import cz.msebera.android.httpclient.entity.StringEntity;
+
+public class RestClient {
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();//used for hmac sha256
+    private static final String BASE_URL = "https://cert.api.firstdata.com/gateway/v2/";
+    private static final String API_KEY = "vL28BOFd8BrFL2XwI54yDYblGNJG9NZk";
+    private static final String API_SECRET = "6Ih3Jy1YuAyGzaXl";
+
+    private AsyncHttpClient client;
+    private Context appContext;
+
+    public RestClient(Context ctx){
+        this.client = new AsyncHttpClient();
+        this.appContext = ctx;
+    }
+
+    public void get(String url, @Nullable JSONObject data, AsyncHttpResponseHandler responseHandler) throws  Exception{
+        String jsonString = data==null?"":data.toString();
+        this.setHeaders(jsonString);
+        client.get(getAbsoluteUrl(url),null,responseHandler);
+    }
+
+    public void post(String url, @Nullable JSONObject data, AsyncHttpResponseHandler responseHandler) throws Exception {
+        String jsonString = data==null?"":data.toString();
+        this.setHeaders(jsonString);
+        StringEntity entity = new StringEntity(jsonString);
+        client.post(this.appContext,getAbsoluteUrl(url),jsonString== ""?null:entity,"application/json",responseHandler);
+    }
+
+    public void post(String url, @Nullable JSONArray data, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), null, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+    private void setHeaders(String jsonString) throws Exception{
+        client.removeAllHeaders();
+        client.addHeader("Content-Type","application/json");
+
+        //HashMap<String,String> map = (HashMap<String, String>)createMessageSignature(jsonString).entrySet();
+
+        for(Map.Entry entry : createMessageSignature(jsonString).entrySet()) {
+            String key = entry.getKey().toString();
+            String value = entry.getValue().toString();
+            client.addHeader(key,value);
+        }
+    }
+
+    private static HashMap<String,String> createMessageSignature(String payload) throws Exception{
+        HashMap<String,String> map =  new HashMap<>();
+        map.put("Client-Request-Id",UUID.randomUUID().toString());
+        map.put("Api-Key",API_KEY);
+        map.put("Timestamp",System.currentTimeMillis()+"");
+        String msg = map.get("Api-Key")+map.get("Client-Request-Id")+
+                map.get("Timestamp")+payload;
+        map.put("Message-Signature",hash_hmac(msg,API_SECRET));
+        return  map;
+    }
+
+    private static String hash_hmac(String str, String secret) throws Exception{
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secretKey);
+        byte[] hashRaw = sha256_HMAC.doFinal(str.getBytes());
+        String hex = bytesToHex(hashRaw).toLowerCase();
+        String aux =Base64.encodeToString(hex.getBytes(),Base64.NO_WRAP);
+        return aux;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
