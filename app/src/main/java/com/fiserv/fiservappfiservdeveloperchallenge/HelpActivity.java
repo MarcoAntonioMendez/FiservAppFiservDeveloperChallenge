d1bf8824c8633703397e3f1ef26107791112e825
Marco Méndez <marco.mendez.ing@gmail.com>
2020-10-25 13:44:51 -0600

    Added functionality in HelpActivity.java, when user touches the e-mail icon, he/she will be able to send en email through the gmail app, when user touches any of the phone numbers, a call will be started.

--- app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/HelpActivity.java
+++ app/src/main/java/com/fiserv/fiservappfiservdeveloperchallenge/HelpActivity.java
@@ -1,17 +1,28 @@
 package com.fiserv.fiservappfiservdeveloperchallenge;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import static android.Manifest.permission.CALL_PHONE;
+
 public class HelpActivity extends AppCompatActivity {
     public static final String NAVIGATION_BAR_COLOR = "#FE3412";
     public static final String STATUS_BAR_COLOR = "#363636";
+    public static final String INSIDE_OF_THE_REPUBLIC_PHONE_NUMBER = "tel:8002155733";
+    public static final String MEXICO_CITY_PHONE_NUMBER = "tel:3311020660";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,97 @@ public class HelpActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getWindow().setNavigationBarColor(Color.parseColor(NAVIGATION_BAR_COLOR));
         getWindow().setStatusBarColor(Color.parseColor(STATUS_BAR_COLOR));
+    }
+
+    /**
+     * When user touches the textview containing the phone number from mexico city, a phone call
+     * will start.
+     * @param view
+     */
+    public void callFromMexicoCity(View view){
+        try
+        {
+            if(Build.VERSION.SDK_INT > 22)
+            {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+
+                    ActivityCompat.requestPermissions(HelpActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(MEXICO_CITY_PHONE_NUMBER));
+                startActivity(callIntent);
+
+            }
+            else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(MEXICO_CITY_PHONE_NUMBER));
+                startActivity(callIntent);
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * When user touches the textview containing phone call from inside of the republic, a phone call
+     * shall start.
+     * @param view -
+     */
+    public void callFromInsideRepublic(View view){
+        try
+        {
+            if(Build.VERSION.SDK_INT > 22)
+            {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+
+                    ActivityCompat.requestPermissions(HelpActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(INSIDE_OF_THE_REPUBLIC_PHONE_NUMBER));
+                startActivity(callIntent);
+
+            }
+            else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(INSIDE_OF_THE_REPUBLIC_PHONE_NUMBER));
+                startActivity(callIntent);
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * When user touches the e-mail icon the gmail app will open allowing the user the send an email
+     * regarding his/her doubts.
+     * @param view - the e-mail icon
+     */
+    public void sendHelpEmail(View view){
+        String[] TO = {"contacto@firstdata.com"};
+        //String[] CC = {"marco.mendez.ing@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Duda sobre los servicios de Fiserv.");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
     }
 
 }
